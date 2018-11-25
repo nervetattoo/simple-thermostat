b14db9415a350188edef34f14fa52fdd2e43540c
Raymond Julin <raymond.julin@gmail.com>
2018-11-25 23:08:14 +0100

    Layout improvements + add mode buttons

--- card.js
+++ card.js
@@ -1,6 +1,5 @@
 import { LitElement, html } from 'https://unpkg.com/@polymer/lit-element@^0.6.1/lit-element.js?module';
 
-
 function renderStyles () {
   return html`
     <style is="custom-style">
@@ -9,36 +8,57 @@ function renderStyles () {
         --thermostat-font-size-xl: var(--paper-font-display3_-_font-size);
         --thermostat-font-size-l: var(--paper-font-display2_-_font-size);
         --thermostat-font-size-m: var(--paper-font-title_-_font-size);
+        --thermostat-font-size-title: 32px;
       }
       div:empty { display: none; }
       .body {
         display: flex;
         flex-direction: row;
-      }
-      .section {
-        width: 50%;
+        justify-content: space-around;
       }
       .main {
         display: flex;
-        flex-direction: column;
+        flex-direction: row;
         align-items: center;
         justify-content: center;
+      }
+      .sensors {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        font-size: 1.1em;
+      }
+      .modes {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+      }
+      .mode {
+        padding: 0;
+        width: 24px;
+        height: 24px;
+        margin: 4px 2px;
+      }
+      .mode.active {
+        color: var(--paper-item-icon-active-color, #FDD835);
       }
       header {
         display: flex;
         justify-content: center;
-        padding: 4px 0;
+        padding: 16px 0 8px 0;
       }
       .title {
-        font-size: var(--thermostat-font-size-m);
+        font-size: var(--thermostat-font-size-title);
+        line-height: var(--thermostat-font-size-title);
         font-weight: normal;
         margin: 0;
         align-self: left;
       }
-      .current {
+      .current-wrapper {
         display: flex;
-        flex-direction: row;
+        flex-direction: column;
         align-items: center;
+        justify-content: center;
       }
       .current--value {
         margin: 0;
@@ -59,7 +79,7 @@ function renderStyles () {
       dl {
         display: flex;
         flex-direction: row;
-        margin-top: 4px;
+        margin-bottom: 6px;
       }
       dt {
         font-weight: 500;
@@ -70,6 +90,19 @@ function renderStyles () {
       }
     </style>
   `
+}
+
+const UPDATE_PROPS = ['entity', 'sensors']
+const modeIcons = {
+  auto: "hass:autorenew",
+  manual: "hass:cursor-pointer",
+  heat: "hass:fire",
+  cool: "hass:snowflake",
+  off: "hass:power",
+  fan_only: "hass:fan",
+  eco: "hass:leaf",
+  dry: "hass:water-percent",
+  idle: "hass:power",
 }
 
 class BetterThermostat extends LitElement {
@@ -104,6 +137,10 @@ class BetterThermostat extends LitElement {
 
   }
 
+  shouldUpdate (changedProps) {
+    return UPDATE_PROPS.some(prop => changedProps.has(prop))
+  }
+
   render ({ _hass, config, entity, sensors } = this) {
     if (!entity) return
     const {
@@ -116,6 +153,7 @@ class BetterThermostat extends LitElement {
       },
     } = entity
     const unit = '℃'
+    //console.log('render', entity)
 
     return html`
       ${renderStyles()}
@@ -125,25 +163,7 @@ class BetterThermostat extends LitElement {
           <h2 class="title">${entity._entityDisplay}</h2>
         </header>
         <section class="body">
-          <div class="main section">
-            <paper-icon-button
-              class="thermostat-trigger"
-              icon="hass:chevron-up"
-              @click='${() => this.setTemperature(desired + .5)}'
-            >
-            </paper-icon-button>
-            <div class="current" @click='${() => this.openEntityPopover(config.entity)}'>
-              <h3 class="current--value">${desired}</h3>
-              <span class="current--unit">${unit}</span>
-            </div>
-            <paper-icon-button
-              class="thermostat-trigger"
-              icon="hass:chevron-down"
-              @click='${() => this.setTemperature(desired - .5)}'
-            >
-            </paper-icon-button>
-          </div>
-          <div class="section info">
+          <div class="section sensors">
             <dl>
               <dt>Temperature:</dt>
               <dd>${current}${unit}</dd>
@@ -157,6 +177,42 @@ class BetterThermostat extends LitElement {
             }) }
 
           </div>
+
+          <div class="modes">
+            ${ operations.map(op => {
+              return html`
+              <paper-icon-button
+                icon="${modeIcons[op]}"
+                title="${op}"
+                class="mode ${op === operation ? 'active' : ''}"
+                @click="${() => this.setMode(op)}"
+              >
+              </paper-icon-button>
+              `
+            })}
+          </div>
+
+          <div class="main section">
+            <div class="current-wrapper">
+              <paper-icon-button
+                class="thermostat-trigger"
+                icon="hass:chevron-up"
+                @click='${() => this.setTemperature(desired + .5)}'
+              >
+              </paper-icon-button>
+
+              <div class="current" @click='${() => this.openEntityPopover(config.entity)}'>
+                <h3 class="current--value">${desired}</h3>
+              </div>
+              <paper-icon-button
+                class="thermostat-trigger"
+                icon="hass:chevron-down"
+                @click='${() => this.setTemperature(desired - .5)}'
+              >
+              </paper-icon-button>
+            </div>
+            <span class="current--unit">${unit}</span>
+          </div>
         </section>
       </ha-card>
     `
@@ -165,8 +221,9 @@ class BetterThermostat extends LitElement {
   renderInfoItem (state, heading) {
     if (!state) return
 
+    const onClick = () => this.openEntityPopover(state.entity_id)
     return html`
-      <dl @click='${() => this.openEntityPopover(state.entity_id)}' >
+      <dl @click='${onClick}' >
         <dt>${heading}:</dt>
         <dd>${state.state} ${state.attributes.unit_of_measurement}</dd>
       </dl>
@@ -175,18 +232,24 @@ class BetterThermostat extends LitElement {
 
   setTemperature (target) {
     let temperature = target
-    console.log(typeof target, target)
+    //console.log(typeof target, target)
     if (typeof target == 'object') {
       temperature = target.temperature
     }
 
-    console.log('set temp', temperature)
+    //console.log('set temp', temperature)
     this._hass.callService("climate", "set_temperature", {
       entity_id: this.config.entity,
       temperature,
     })
   }
 
+  setMode (mode) {
+     this._hass.callService("climate", "set_operation_mode", {
+      entity_id: this.config.entity,
+      operation_mode: mode,
+    });
+  }
 
   openEntityPopover (entityId) {
     this.fire('hass-more-info', { entityId });
