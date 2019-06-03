065c53f570f796d3070d5de7a31aedffd5cb9812
Raymond Julin <raymond.julin@gmail.com>
2019-06-03 15:12:15 +0200

    Also minify tagged template literals and shave of a few more bytes

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import resolve from 'rollup-plugin-node-resolve'
 import { terser } from 'rollup-plugin-terser'
 import filesize from 'rollup-plugin-filesize'
 import commonjs from 'rollup-plugin-commonjs'
+import minifyHTML from 'rollup-plugin-minify-html-literals'
 
 export default {
   input: 'src/index.js',
@@ -10,5 +11,5 @@ export default {
     format: 'umd',
     name: 'SimpleThermostat',
   },
-  plugins: [resolve(), commonjs(), terser(), filesize()],
+  plugins: [resolve(), commonjs(), minifyHTML(), terser(), filesize()],
 }
