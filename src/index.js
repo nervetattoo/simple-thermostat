05feab622fb1a597e840c615bd5ec05762c27442
Raymond Julin <raymond.julin@gmail.com>
2019-10-15 22:51:00 +0200

    Fix icon/name override for individual modes. Fixes #72

--- src/index.js
+++ src/index.js
@@ -193,8 +193,9 @@ class SimpleThermostat extends LitElement {
       this.modeOptions.names = _names
       this.modeOptions.icons = _icons
 
-      if (modes.length > 0) {
-        controlModes = Object.entries(modes)
+      const entries = Object.entries(modes)
+      if (entries.length > 0) {
+        controlModes = entries
           .filter(([type]) => supportedModeType(type))
           .map(([type, config]) => {
             return {
